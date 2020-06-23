2274800f9288d678939de9c07c8a20513f969e53
Kay <ibotforwin@gmail.com>
2020-06-23 02:51:28 -0400

    Added a mobile menu using the react-device-detect library.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,13 @@
 /* @flow */
-import React, { CSSProperties } from "react";
+import React, { CSSProperties, useState } from "react";
 import symbol from "../../symbol";
 import { Button } from "../atom/Button";
 import Space from "../atom/Space";
 import { Title } from "../atom/Text";
 import Divider from "../atom/Divider";
 import { useStore } from "../../store";
+import {BrowserView, MobileView, isBrowser, isMobile} from "react-device-detect"
+import {Menu, MenuItem} from "@material-ui/core";
 
 type StyleKey = "header" | "titleContainer" | "buttonContainer";
 
@@ -28,20 +30,59 @@ const styles: Record<StyleKey, CSSProperties> = {
 };
 
 const Header = () => {
+  const [anchorEl, setAnchorE1]=useState(false)
+
+  const handleClick=()=>{
+    setAnchorE1(true)
+  }
+  const handleClose=()=>{
+    setAnchorE1(false)
+  }
   const { actions } = useStore();
   const showInfoModal = () => {
     actions.showInfoModal();
+    setAnchorE1(false)
   };
   const showDonateModal = () => {
     actions.showDonateModal();
+    setAnchorE1(false)
   };
   const showContactModal = () => {
     actions.showContactModal();
+    setAnchorE1(false)
   };
   return (
+<div>
+<MobileView>
+  <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+    Menu
+  </Button>
+  <Menu
+      id="simple-menu"
+      getContentAnchorEl={null}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "left"
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "left"
+      }}
+      keepMounted
+      anchorEl={anchorEl}
+      open={Boolean(anchorEl)}
+      onClose={handleClose}
+  >
+    <MenuItem onClick={showInfoModal}>ABOUT</MenuItem>
+    <MenuItem onClick={showContactModal}>CONTACT</MenuItem>
+    <MenuItem onClick={showDonateModal}>DONATE</MenuItem>
+  </Menu>
+</MobileView>
+
+<BrowserView>
     <Space.Inset style={styles.header} vertical="medium" horizontal="large">
       <div style={styles.titleContainer}>
-        <Title>VISAGE VOID</Title>
+        <Title><div className='h3'>VISAGE VOID</div></Title>
       </div>
       <div style={styles.buttonContainer}>
         <Button onClick={showInfoModal}>ABOUT</Button>
@@ -55,6 +96,7 @@ const Header = () => {
         <Button onClick={showDonateModal}>DONATE</Button>
       </div>
     </Space.Inset>
+</BrowserView></div>
   );
 };
 
