25dbae11c7e603e2a7838c29de07d4ab239a76c1
Hirokazu Tei <hirokazutei@nextdoor.com>
2020-06-24 08:45:39 -0400

    ii

--- src/const.tsx
+++ src/const.tsx
@@ -3,7 +3,9 @@ import { CoverType } from "./types";
 export const COVER_TYPE: Array<CoverType> = ["BOX", "OVAL", "EMOJI"];
 
 export const FACE_EMOJIS = [
-  "😀",  "😃",  "😄",
+  "😀",
+  "😃",
+  "😄",
   "😁",
   "😆",
   "😅",
